e12dac7ace5cf8670a3b26c2689689a016e4d66d
Stephan Wiesenhütter <stephan.wiesenhuetter@eulitha.com>
2022-01-03 17:16:18 +0100

    compiles __init__.py as well

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+python setup.py clean --all
 python setup.py bdist_wheel 
 
 
--- setup.py
+++ setup.py
@@ -1,29 +1,51 @@
 from setuptools import setup, find_packages
+from distutils.command.build_ext import build_ext
 from setuptools.command.build_py import build_py
 from Cython.Build import cythonize
 from Cython.Compiler import Options
+import sys
+
 
 with open("README.md", 'r') as f:
     long_description = f.read()
 
-files = ["main.py", "gui/gui.py"]
-cython_excludes = ["**/__init__.py"]
-
 Options.docstrings = False  # remove comments
 
-
 class skip_py(build_py):
+    """
+    overwrites distutils code to skip py source installation
+    without this we would distribute *.py sources
+    """
     def find_package_modules(self, package, package_dir):
-        modules = super().find_package_modules(package, package_dir)
-        init_files = list(filter(lambda x: "__init__.py" == x[1], modules))
-        return init_files
+        return []
 
 
-compiled = cythonize(files,
-    exclude=cython_excludes,
-    build_dir="build",
-    force=True,
-    compiler_directives={'language_level': "3"})
+class build_ext_with_init(build_ext):
+    """
+    overwrite distutils code to fix the "__init__.py" issue
+    see https://bugs.python.org/issue35893
+    and 
+    https://stackoverflow.com/questions/58797673/how-to-compile-init-py-file-using-cython-on-windows    
+    """
+    def get_export_symbols(self, ext):
+        return []
+
+
+"""
+no cythonize when cleaning
+cythonize creates the *.c-sources
+"""
+if 'clean' in sys.argv:
+    compiled = []
+else:
+    files = ["**/*.py"]
+    cython_excludes = ["setup.py"]
+    compiled = cythonize(files,
+        exclude=cython_excludes,
+        build_dir="build",
+        language="c++",
+        force=True,
+        compiler_directives={'language_level': "3"})
 
 setup(
     author="Eulitha AG",
@@ -36,7 +58,7 @@ setup(
     install_requires=["PySide6"],
     setup_requires=["setuptools", "cython", "wheel"],
     ext_modules=compiled,
-    cmdclass={'build_py': skip_py},
+    cmdclass={'build_ext': build_ext_with_init, 'build_py': skip_py},
     entry_points={
         'console_scripts': [
             'eulitha = gui:main'
